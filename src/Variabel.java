c7bcd9f702e408ace95b0a1178cb854e18f0caba
yusufva <yusufvalent@gmail.com>
2024-02-28 11:42:33 +0700

    feature: creating Variabel Class

---
+++ src/Variabel.java
@@ -0,0 +1,10 @@
+public class Variabel {
+    public void PrintVariabel(){
+        var a = "ini strings";
+        var b = 100;
+        var c = true;
+        System.out.println("variabel string: " + a);
+        System.out.println("variabel integer: " + b);
+        System.out.println("variabel boolean: " + c);
+    }
+}
