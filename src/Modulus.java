68874712cee978db53501e1be11493df4e86698e
yusufva <yusufvalent@gmail.com>
2024-02-29 12:04:51 +0700

    feature: Adding Modulus align number Class and method

--- src/Modulus.java
+++ src/Modulus.java
@@ -1,3 +1,22 @@
 public class Modulus {
+    public int n;
 
+    public Modulus(int n){
+        this.n = n;
+    }
+
+    public void printNumber(){
+        int n = this.n;
+        int count = 0;
+        int i = 1;
+        while(count<n){
+            if(i%3==0&& i%2!=0){
+                System.out.print(i+" ");
+                count += 1;
+                i +=1;
+            } else{
+                i+=1;
+            }
+        }
+    }
 }
